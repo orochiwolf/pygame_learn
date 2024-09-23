bc0b634fd88aafb17a672cb8002367be55e64cb6
Said MOUHOUN <s.mouhoun@gmail.com>
2024-09-24 00:05:37 +0100

    my first game

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame, sys
+
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HIGH = 800
+window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIGH))
+pygame.display.set_caption("MY first pygame game")
+clock = pygame.time.Clock()
+#game loop
+while True : 
+    #Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.quit()
+
+    #Update position
+
+    #Draw the change
+    pygame.display.update()
+    clock.tick(60)
