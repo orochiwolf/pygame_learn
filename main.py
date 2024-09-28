7fbfc914f05c6dd2a543e56e32a902675fdff38e
Said MOUHOUN <s.mouhoun@gmail.com>
2024-09-28 12:09:12 +0100

    add mouvement to the ball in the game, and managing the screnn limit

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pygame, sys
 pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HIGH = 800
+GREEN = (20, 160, 133)
+BACKGROUND_COLOR = GREEN
+
+ball_raduis = 10
+ball_x = 400
+ball_y = 400
+ball_speed_x = 0
+ball_speed_y = 0
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIGH))
 pygame.display.set_caption("MY first pygame game")
 clock = pygame.time.Clock()
@@ -12,12 +20,36 @@ while True :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ball_speed_x = 3
+            elif event.key == pygame.K_LEFT:
+                ball_speed_x = -3
+            elif event.key == pygame.K_UP:
+                ball_speed_y = -3
+            elif event.key == pygame.K_DOWN:
+                ball_speed_y = 3
 
     #Update position
+    if ball_x  >= SCREEN_HIGH + ball_raduis :
+        ball_x = 0
+    if ball_x  < 0:
+        ball_x = SCREEN_HIGH
+    if ball_y < 0:
+        ball_y = SCREEN_WIDTH
+    if ball_y > SCREEN_WIDTH:
+        ball_y = 0
+    else:
+        ball_x += ball_speed_x
+        ball_y += ball_speed_y
+
+
+
 
     #Draw the change
-    pygame.draw.circle(window, pygame.Color('white'), (400, 400), 20)
+    window.fill(BACKGROUND_COLOR)
+    pygame.draw.circle(window, pygame.Color('white'), (ball_x, ball_y), ball_raduis)
 
 
     pygame.display.update()
